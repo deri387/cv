24816be98f441f9e11faabf3af9877fde7d5cee0
deri387 <37043134+deri387@users.noreply.github.com>
2018-09-22 13:30:19 +0700

    Create url.js

---
+++ url.js
@@ -0,0 +1,6 @@
+{
+  "listberita": [
+    "http://www.ajputramandiri.com/berita-7-history-of-straw.html",
+     "http://www.sdn-purwobinangun.sch.id/berita-9-jk-ingin-semua-guru-harus-jadi-pns-nasional-ini-alasannya-.html"
+  ]
+}
