ff37ce2ae33bbcb048b438af8b9faac89454584f
deri387 <37043134+deri387@users.noreply.github.com>
2018-09-26 14:56:51 +0700

    Update url.js

--- url.js
+++ url.js
@@ -3,7 +3,6 @@
     
     "http://www.sdn-purwobinangun.sch.id/berita-9-jk-ingin-semua-guru-harus-jadi-pns-nasional-ini-alasannya-.html",
     "http://www.selorejohotel.com/berita-145-kemah-nasional-kesehatan-2016.html",
-    "http://www.knpibengkaliskab.org/berita-233-pk-knpi-mandau-gelar--tournament-bulutangkis.html",
     "http://www.setdakabsubang.com/berita-69-gelombang-solidaritas-untuk-palestina.html",
     "http://www.smkn1polewali.sch.id/berita-ujian-sekolah-2016.html",
     "http://www.smkntempeh-lmj.sch.id/berita-pengumuman-kelulusan-2018.html",
