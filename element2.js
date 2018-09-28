525d78a6fa093125a3347b0f1096c8517fa864cf
deri387 <37043134+deri387@users.noreply.github.com>
2018-09-28 16:39:43 +0700

    Create element2.js

---
+++ element2.js
@@ -0,0 +1,402 @@
+var wbwithspace = [
+        'diwul.com',
+        'dewiasmaraku.com',
+        'barberfox21.com',
+        'lkpikma.com',
+        'jeligamatjogjakarta.com',
+        'man2kabgorontalo.com',
+        'stifibp.ac.id',
+        'smp1semboro.sch.id',
+        'mediaaudio.id',
+        'mtswikebarongan.sch.id',
+        'smpnegeri3kediri.sch.id',
+        'smkwikarya.com',
+        'setdakabsubang.com',
+        'riauberdaulat.com',
+        'smkn1pringgasela.sch.id',
+        'smansasak.sch.id',
+        'www.smanuela.sch.id',
+        '85retaildesign.com',
+        'lpjm.stikes-mw.ac.id'
+    ];
+    var leftc = [
+        'keltbkota3.tanjungbalaikota.go.id',
+        'keltbkota1.tanjungbalaikota.go.id',
+        'kelpantaijohor.tanjungbalaikota.go.id',
+        'kelsijambi.tanjungbalaikota.go.id',
+        'keltbkota2.tanjungbalaikota.go.id',
+        'kelsejahtera.tanjungbalaikota.go.id',
+        'keltbkota4.tanjungbalaikota.go.id',
+        'keltanjungmedan.tanjungbalaikota.go.id',
+        'kelsumbersari.tanjungbalaikota.go.id',
+        'kelsemulajadi.tanjungbalaikota.go.id',
+        'kelselatlancang.tanjungbalaikota.go.id',
+        'kelsirantau.tanjungbalaikota.go.id',
+        'kelkualasilaubestari.tanjungbalaikota.go.id',
+        'kelbungatanjung.tanjungbalaikota.go.id',
+        'kelpulausimardan.tanjungbalaikota.go.id',
+        'kelmuarasentosa.tanjungbalaikota.go.id',
+        'kelgading.tanjungbalaikota.go.id',
+        'kelseiraja.tanjungbalaikota.go.id',
+        'kelpahang.tanjungbalaikota.go.id',
+        'kelpasarbaru.tanjungbalaikota.go.id',
+        'kelkarya.tanjungbalaikota.go.id',
+        'kelkapiaspulaubuaya.tanjungbalaikota.go.id',
+        'kelmatahalasan.tanjungbalaikota.go.id',
+        'kelperjuangan.tanjungbalaikota.go.id',
+        'kelbetingkualakapias.tanjungbalaikota.go.id',
+        'kelkeramatkubah.tanjungbalaikota.go.id',
+        'kelpantaiburung.tanjungbalaikota.go.id',
+        'kelpematangpasir.tanjungbalaikota.go.id',
+        'kelperwira.tanjungbalaikota.go.id',
+        'kelindrasakti.tanjungbalaikota.go.id',
+        'piikalsel.org',
+        'alsakomputer.com',
+        'smkwikarya.com',
+        'smpn1belawa.90op.com',
+        'rumahdai.com',
+        'amico-amc.16mb.com',
+        'buletin.lpkhi.com',
+        'taufiqahmad.byethost14.com',
+    ];
+        try {
+            $('body > div.boxed > div.header > div.header-menu.thisisfixed.floatingmainmenu').remove();
+        } catch (err) {}
+        try {
+            $('body > div.col-xs-12').remove();
+        } catch (err) {}
+
+        try {
+            $('.nav').remove();
+        } catch (err) {}
+        try {
+            $('.navbar').remove();
+        } catch (err) {}
+        try {
+            $('.header-background').remove();
+        } catch (err) {}
+        try {
+            $('.widget').remove();
+        } catch (err) {}
+        try {
+            $('.search').remove();
+        } catch (err) {}
+        try {
+            $('.emerald').remove();
+        } catch (err) {}
+        try {
+            $('.menu').remove();
+        } catch (err) {}
+        try {
+            $('.navigation').remove();
+        } catch (err) {}
+        try {
+            $('.navbar-vertical').remove();
+        } catch (err) {}
+        try {
+            $('.top-line').remove();
+        } catch (err) {}
+        try {
+            $('.sidebar').remove();
+        } catch (err) {}
+        try {
+            $('.art-nav').remove();
+        } catch (err) {}
+        try {
+            $('.art-header').remove();
+        } catch (err) {}
+        try {
+            $('.art-sidebar1').remove();
+        } catch (err) {}
+        try {
+            $('.rotating_banner').remove();
+        } catch (err) {}
+        try {
+            $('.rotating_text').remove();
+        } catch (err) {}
+        try {
+            $('.main-header').remove();
+        } catch (err) {}
+        try {
+            $('.breaking-wrapper').remove();
+        } catch (err) {}
+        try {
+            $('.main-content-slider').remove();
+        } catch (err) {}
+        try {
+            $('.small-nav').remove();
+        } catch (err) {}
+        try {
+            $('.ui-tabs-nav').remove();
+        } catch (err) {}
+        try {
+            $('.header').remove();
+        } catch (err) {}
+        try {
+            $('.search-container').remove();
+        } catch (err) {}
+        try {
+            $('.grid_12').remove();
+        } catch (err) {}
+        try {
+            $('.breaking-news').remove();
+        } catch (err) {}
+        try {
+            $('.quick-contact').remove();
+        } catch (err) {}
+        try {
+            $('.col1').remove();
+        } catch (err) {}
+        try {
+            $('.tabssec').remove();
+        } catch (err) {}
+        try {
+            $('.featured_news').remove();
+        } catch (err) {}
+        try {
+            $('.crumb').remove();
+        } catch (err) {}
+
+        try {
+            $('#top_header').remove();
+        } catch (err) {}
+        try {
+            $('#banner').remove();
+        } catch (err) {}
+        try {
+            $('#top-ticker').remove();
+        } catch (err) {}
+        try {
+            $('#menuutama').remove();
+        } catch (err) {}
+        try {
+            $('#headline').remove();
+        } catch (err) {}
+        try {
+            $('#bottom_header').remove();
+        } catch (err) {}
+        try {
+            $('#sidebar').remove();
+        } catch (err) {}
+        try {
+            $('#sidebar1').remove();
+        } catch (err) {}
+        try {
+            $('#logo').remove();
+        } catch (err) {}
+        try {
+            $('#platform-menu').remove();
+        } catch (err) {}
+        try {
+            $('#bar-menu').remove();
+        } catch (err) {}
+        try {
+            $('#slide-frame').remove();
+        } catch (err) {}
+
+        try {
+            $('#header').remove();
+        } catch (err) {}
+        try {
+            $('#nav').remove();
+        } catch (err) {}
+        try {
+            $('#main-navigation').remove();
+        } catch (err) {}
+        try {
+            $('#top-navigation').remove();
+        } catch (err) {}
+        try {
+            if (window.location.hostname.includes('jamil.unkhair') == false)
+                $('#top').remove();
+        } catch (err) {}
+
+        try {
+            $('#kiri').remove();
+        } catch (err) {}
+        try {
+            $('#main-menu-container').remove();
+        } catch (err) {}
+        try {
+            $('#logobanner').remove();
+        } catch (err) {}
+        var found1 = false;
+        //console.log(leftc)
+        for (var i = 0; i < leftc.length && !found1; i++) {
+            var cdomain = window.location.hostname;
+            cdomain = cdomain.replace('www.', '');
+            //console.log(cdomain);
+            if (leftc.includes(cdomain)) {
+                found1 = true;
+                break;
+            }
+        }
+        //console.log(found1);
+        if (found1 == false) {
+            try {
+                $('#leftcontent').remove();
+            } catch (err) {
+                //console.log(err);
+            }
+        }
+
+        try {
+            $('#content-kiri-headline').remove();
+        } catch (err) {}
+
+        try {
+            $('.content').focus();
+        } catch (err) {}
+setTimeout(function(){
+try {
+                var nama = document.getElementsByName('nama');
+                var count_nama = nama.length;
+                for (x = 0; x <= count_nama; x++) {
+                    var kwkom2 = '' + tNama.Text + ''; 
+                    if (kwkom2 && kwkom2 !== undefined) {
+                        nama[x].value = '{0}';
+                    }
+                }
+            } catch (err) {}
+
+            try {
+                var nama_komentar = document.getElementsByName('nama_komentar');
+                var count_nama_komentar = nama_komentar.length;
+                for (x = 0; x <= count_nama_komentar; x++) {
+                    var kwkom2 = '{0}';
+                    if (kwkom2 && kwkom2 !== undefined) {
+                        nama_komentar[x].value = '{0}';
+                    }
+                }
+            } catch (err) {}
+
+            try {
+                var url_input = document.getElementsByName('url');
+                var count_url_input = url_input.length;
+                var found = false;
+                for (var i = 0; i < wbwithspace.length && !found; i++) {
+                    var cekdom = window.location.hostname;
+                    cekdom = cekdom.replace('www.', '');
+                    if (wbwithspace.includes(cekdom)) {
+                        found = true;
+                        break;
+                    }
+                }
+
+                if (found == true) {
+                    var cekurl = '{1}';
+                    if (cekurl && cekurl !== undefined) {
+                        cekurl = '{1}';
+                    } else {
+                        cekurl = '';
+                    }
+
+                    var thedom = '';
+
+                    if (cekurl.includes('http') == false) {
+                        thedom = 'http://' + cekurl;
+                    }
+
+                    for (x = 0; x <= count_url_input; x++) {
+                        url_input[x].value = '                     ' +
+                            thedom;
+                    }
+                } else {
+                    for (x = 0; x <= count_url_input; x++) {
+                        var cekurl = '{1}';
+                        if (cekurl && cekurl !== undefined) {
+                            url_input[x].value = '{1}';
+                        }
+                    }
+                }
+
+            } catch (err) {}
+
+            try {
+                var wb_input = document.getElementsByName('website');
+                var count_wb_input = wb_input.length;
+                for (x = 0; x <= count_wb_input; x++) {
+                    var cekurl = '{1}';
+                    if (cekurl && cekurl !== undefined) {
+                        wb_input[x].value = '{1}';
+                    }
+                }
+            } catch (err) {}
+
+            try {
+                var email_input = document.getElementsByName('email');
+                var count_email_input = email_input.length;
+                for (x = 0; x <= count_email_input; x++) {
+                    var cekurl = '{1}';
+                    if (cekurl && cekurl !== undefined) {
+                        email_input[x].value = '{1}';
+                    }
+                }
+            } catch (err) {}
+
+            try {
+                var pesan_input = document.getElementsByName('pesan');
+                var count_pesan_input = pesan_input.length;
+                for (x = 0; x <= count_pesan_input; x++) {
+                    var kom2 = '{2}';
+                    if (kom2 && kom2 !== undefined) {
+                        pesan_input[x].value = '{2} <a href="https://{1}">{0}</a>';
+                    }
+                }
+            } catch (err) {}
+
+            try {
+                var isi_komentar_input = document.getElementsByName('isi_komentar');
+                var count_isi_komentar_input = isi_komentar_input.length;
+                for (x = 0; x <= count_isi_komentar_input; x++) {
+                    var kom2 = '{2}';
+                    if (kom2 && kom2 !== undefined) {
+                        isi_komentar_input[x].value = '{2}';
+                    }
+                }
+            } catch (err) {}
+
+            try {
+                var isi_kom_input = document.getElementsByName('isi_kom');
+                var count_isi_kom_input = isi_kom_input.length;
+                for (x = 0; x <= count_isi_kom_input; x++) {
+                    var kom2 = '{2}';
+                    if (kom2 && kom2 !== undefined) {
+                        isi_kom_input[x].value = '{2}';
+                    }
+                }
+            } catch (err) {}
+
+            try {
+                var isi_koment_input = document.getElementsByName('isi_koment');
+                var count_isi_koment_input = isi_koment_input.length;
+                for (x = 0; x <= count_isi_koment_input; x++) {
+                    var kom2 = '{2}';
+                    if (kom2 && kom2 !== undefined) {
+                        isi_koment_input[x].value = '{2}';
+                    }
+                }
+            } catch (err) {}
+
+            try {
+                var isi_input = document.getElementsByName('isi');
+                var count_isi_input = isi_input.length;
+                for (x = 0; x <= count_isi_input; x++) {
+                    var kom2 = '{2}';
+                    if (kom2 && kom2 !== undefined) {
+                        isi_input[x].value = '{2}';
+                    }
+                }
+            } catch (err) {}
+
+            try {
+                var komentar_input = document.getElementsByName('komentar');
+                var count_komentar_input = komentar_input.length;
+                for (x = 0; x <= count_komentar_input; x++) {
+                    var kom2 = '{2}';
+                    if (kom2 && kom2 !== undefined) {
+                        komentar_input[x].value = '{2}';
+                    }
+                }
+            } catch (err) {}
+},500);
